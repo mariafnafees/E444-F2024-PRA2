2b1db9da12169b8173912164a8132f4dbc70e953
Maria Nafees <mariafnafees@gmail.com>
2024-09-23 17:03:30 -0400

    updated all template files and completed activity 1.3, weppage now looks like provided example.

--- hello.py
+++ hello.py
@@ -1,14 +1,34 @@
+from datetime import datetime
 from flask import Flask, render_template
-#from flask_bootstrap import Bootstrap
 from flask_sqlalchemy import SQLAlchemy
+from flask_moment import Moment
 
 app = Flask(__name__)
 
+moment = Moment(app)
+
+@app.errorhandler(404)
+def page_not_found(e):
+    app.logger.error(f"Page not found: {e}")  # Logging the error
+    return render_template('404.html'), 404
+
+@app.errorhandler(500)
+def internal_server_error(e):
+    app.logger.error(f"Internal server error: {e}")  # Logging the error
+    return render_template('500.html'), 500
+
 @app.route('/')
 def index():
-    return render_template('index.html')
-
+    current_time = datetime.utcnow()
+    app.logger.info(f"Index page accessed at {current_time}")  # Logging access
+    return render_template('index.html',
+                           current_time=current_time)
 
 @app.route('/user/<name>')
 def user(name):
-    return render_template('user.html', name=name)
+    current_time = datetime.utcnow()
+    app.logger.info(f"User page for {name} accessed at {current_time}")  # Logging access
+    return render_template('user.html', name=name, current_time=current_time)
+
+if __name__ == "__main__":
+    app.run(debug=True)
