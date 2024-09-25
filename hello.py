5fcd0ee19e25a08e9ec9cef17adae5db1e4c25ee
Maria Nafees <mariafnafees@gmail.com>
2024-09-24 20:22:58 -0400

    Updated example to include email field, need to update error messages.

--- hello.py
+++ hello.py
@@ -1,21 +1,25 @@
 from flask import Flask, render_template, session, redirect, url_for, flash
 from flask_bootstrap import Bootstrap
 from flask_moment import Moment
+from datetime import datetime
 from flask_wtf import FlaskForm
 from wtforms import StringField, SubmitField
-from wtforms.validators import DataRequired
+from wtforms.validators import DataRequired, ValidationError
 
 app = Flask(__name__)
-app.config['SECRET_KEY'] = 'hard to guess string'
-
 bootstrap = Bootstrap(app)
 moment = Moment(app)
-
-
+app.config['SECRET_KEY'] = 'change me'
 class NameForm(FlaskForm):
     name = StringField('What is your name?', validators=[DataRequired()])
-    submit = SubmitField('Submit')
-
+    email = StringField('What is your UofT email address?', validators=[DataRequired()])
+    submit = SubmitField('Submit') 
+    def validate_email(form, field):
+        if "@" not in field.data:
+            raise ValidationError('Please include an \'@\' in the email address.')
+        if "utoronto" not in field.data:
+            raise ValidationError('Must be a UofT email address.')
+        
 
 @app.errorhandler(404)
 def page_not_found(e):
@@ -26,7 +30,6 @@ def page_not_found(e):
 def internal_server_error(e):
     return render_template('500.html'), 500
 
-
 @app.route('/', methods=['GET', 'POST'])
 def index():
     form = NameForm()
@@ -35,5 +38,13 @@ def index():
         if old_name is not None and old_name != form.name.data:
             flash('Looks like you have changed your name!')
         session['name'] = form.name.data
+        session['email'] = form.email.data
         return redirect(url_for('index'))
-    return render_template('index.html', form=form, name=session.get('name'))
+    return render_template('index.html', form=form, name=session.get('name'), email=session.get('email'))
+
+@app.route('/user/<name>')
+def user(name):
+ return render_template('user.html', name=name, current_time=datetime.utcnow())
+
+if __name__ == '__main__':
+    app.run(debug=True)
