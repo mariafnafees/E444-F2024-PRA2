adf9a2e779a2f532d82ef0d5c9a36c14959a22a4
Maria Nafees <mariafnafees@gmail.com>
2024-09-24 20:09:50 -0400

    Completed first part of activity 1.4 by reproducing example 4-7 with flashing messages.

--- hello.py
+++ hello.py
@@ -1,34 +1,39 @@
-from datetime import datetime
-from flask import Flask, render_template
-from flask_sqlalchemy import SQLAlchemy
+from flask import Flask, render_template, session, redirect, url_for, flash
+from flask_bootstrap import Bootstrap
 from flask_moment import Moment
+from flask_wtf import FlaskForm
+from wtforms import StringField, SubmitField
+from wtforms.validators import DataRequired
 
 app = Flask(__name__)
+app.config['SECRET_KEY'] = 'hard to guess string'
 
+bootstrap = Bootstrap(app)
 moment = Moment(app)
+
+
+class NameForm(FlaskForm):
+    name = StringField('What is your name?', validators=[DataRequired()])
+    submit = SubmitField('Submit')
+
 
 @app.errorhandler(404)
 def page_not_found(e):
-    app.logger.error(f"Page not found: {e}")  # Logging the error
     return render_template('404.html'), 404
+
 
 @app.errorhandler(500)
 def internal_server_error(e):
-    app.logger.error(f"Internal server error: {e}")  # Logging the error
     return render_template('500.html'), 500
 
-@app.route('/')
+
+@app.route('/', methods=['GET', 'POST'])
 def index():
-    current_time = datetime.utcnow()
-    app.logger.info(f"Index page accessed at {current_time}")  # Logging access
-    return render_template('index.html',
-                           current_time=current_time)
-
-@app.route('/user/<name>')
-def user(name):
-    current_time = datetime.utcnow()
-    app.logger.info(f"User page for {name} accessed at {current_time}")  # Logging access
-    return render_template('user.html', name=name, current_time=current_time)
-
-if __name__ == "__main__":
-    app.run(debug=True)
+    form = NameForm()
+    if form.validate_on_submit():
+        old_name = session.get('name')
+        if old_name is not None and old_name != form.name.data:
+            flash('Looks like you have changed your name!')
+        session['name'] = form.name.data
+        return redirect(url_for('index'))
+    return render_template('index.html', form=form, name=session.get('name'))
